da2c454902775a8eb596cb718057a7f0f9eb1b8f
Luke Szanto <luke@livepro.com.au>
2017-04-13 21:05:09 +1000

    Add user create function

--- handlers/user.go
+++ handlers/user.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	validator "gopkg.in/go-playground/validator.v9"
+
 	"github.com/jinzhu/gorm"
+	"github.com/lszanto/links/models"
 	"github.com/lszanto/multusbe/response"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // UserHandler handles user activity
@@ -15,6 +20,26 @@ type UserHandler struct {
 // NewUserHandler creates a new user handler
 func NewUserHandler(db *gorm.DB) UserHandler {
 	return UserHandler{DB: db}
+}
+
+// Create makes a new user
+func (handler UserHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+
+	validate := validator.New()
+
+	err := validate.Struct(user)
+	if err != nil {
+		response.JSON(w, response.Result{Result: "Error creating user"}, http.StatusNotAcceptable)
+		return
+	}
+
+	hash, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	user.Password = string(hash)
+
+	handler.DB.Create(&user)
+	response.JSON(w, response.Result{Result: "User created"}, http.StatusCreated)
 }
 
 // Login allows the user to login
