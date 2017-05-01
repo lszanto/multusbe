2b33678457921bb287fa632a38f52c7ada2d7d5d
Luke Szanto <luke@livepro.com.au>
2017-05-01 22:32:07 +1000

    Fix up create func and begin work on login

--- handlers/user.go
+++ handlers/user.go
@@ -3,11 +3,12 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	validator "gopkg.in/go-playground/validator.v9"
 
 	"github.com/jinzhu/gorm"
-	"github.com/lszanto/links/models"
+	"github.com/lszanto/multusbe/models"
 	"github.com/lszanto/multusbe/response"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -31,18 +32,44 @@ func (handler UserHandler) Create(w http.ResponseWriter, r *http.Request) {
 
 	err := validate.Struct(user)
 	if err != nil {
-		response.JSON(w, response.Result{Result: "Error creating user"}, http.StatusNotAcceptable)
+		response.JSON(w, response.Result{Error: "Error creating user"}, http.StatusNotAcceptable)
 		return
 	}
 
 	hash, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	user.Password = string(hash)
 
-	handler.DB.Create(&user)
+	if err := handler.DB.Create(&user).Error; err != nil {
+		errorMessage := "Error creating user"
+
+		// if we've got a duplicate entry from the email then update message accordingly
+		if strings.Contains(err.Error(), "Duplicate entry") {
+			errorMessage = "A user with this email already exists"
+		}
+
+		response.JSON(w, response.Result{Error: errorMessage}, http.StatusNotAcceptable)
+		return
+	}
 	response.JSON(w, response.Result{Result: "User created"}, http.StatusCreated)
 }
 
 // Login allows the user to login
 func (handler UserHandler) Login(w http.ResponseWriter, r *http.Request) {
-	response.JSON(w, "hello", http.StatusOK)
+	var attempt models.User
+	json.NewDecoder(r.Body).Decode(&attempt)
+
+	var user models.User
+
+	if err := handler.DB.Where("username = ?", attempt.Username).First(&user).Error; err != nil {
+		response.JSON(w, response.Result{Error: "Could not login"}, http.StatusNotFound)
+		return
+	}
+
+	// check that hashes match
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(attempt.Password)); err != nil {
+		response.JSON(w, response.Result{Error: "Coult not login"}, http.StatusNotFound)
+		return
+	}
+
+	response.JSON(w, response.Result{Result: "Logged in"}, http.StatusCreated)
 }
