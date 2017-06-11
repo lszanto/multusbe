37460b9acd65e286c52d96d46dc314e037eb5dcc
Luke Szanto <luke@livepro.com.au>
2017-06-11 21:32:06 +1000

    Add login response type with JWT token

--- response/types.go
+++ response/types.go
@@ -5,3 +5,8 @@ type Result struct {
 	Result interface{} `json:"result,omitempty"`
 	Error  interface{} `json:"error,omitempty"`
 }
+
+// LoginResult holds a JWT token
+type LoginResult struct {
+	Token string `json:"token,omitempty"`
+}
